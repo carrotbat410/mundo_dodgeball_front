9e705f1d4c0dcca6822ade97586564c52e7bfc9c
carrotbat410 <carrotbat410@gmail.com>
2024-10-19 15:59:37 +0900

    글자 페이지 만듦

--- src/pages/Typography.vue
+++ src/pages/Typography.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Typography Page</div>
+  <q-page class="q-pa-xl">
+    <section>
+      <div>Typography Page</div>
+      <div class="text-h4">Headings</div>
+      <p class="text-h1">Headline 1</p>
+      <p class="text-h2">Headline 2</p>
+      <p class="text-h3">Headline 3</p>
+      <p class="text-h4">Headline 4</p>
+      <p class="text-h5">Headline 5</p>
+      <p class="text-h6">Headline 6</p>
+      <p class="text-subtitle1">Subtitle 1</p>
+      <p class="text-subtitle2">Subtitle 2</p>
+      <p class="text-body1">body 1 asdahvosahvosadof</p>
+      <p class="text-body2">body 2 asdahvosahvosadof</p>
+      <p class="text-caption">caption asdjsakldjsad</p>
+      <p class="text-overline">overline sdfdsiofhasoifhsd</p>
+    </section>
+  </q-page>
 </template>
 
 <script setup></script>
